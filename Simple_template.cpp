28148d9350f38fe2a8a2d9a0bc251b2c165e2301
sorachandu <sora.chandu28@gmail.com>
2025-03-27 10:52:15 +0900

    Simple_template.cpp 追加

---
+++ Simple_template.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,l,r) for(int i=(l);i<(r);i++)
+using ll=long long;
+template<typename T1,typename T2>inline bool chmin(T1 &a,T2 b){ return (a>b?a=b,true:false); }
+template<typename T1,typename T2>inline bool chmax(T1 &a,T2 b){ return (a<b?a=b,true:false); }
+int main(){
+    cin.tie(nullptr)->ios::sync_with_stdio(false); 
+    
+}
