2f934d4c12a97b8f5d26166bdffec7cf80e1aeb8
sorachandu <sora.chandu28@gmail.com>
2025-04-21 18:45:14 +0900

    MultipleSort.cpp追加 short_templates.cpp古いの消した

---
+++ MultipleSort.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// This function sorts multiple vectors based on the first vector
+// and returns the indices of the sorted order.
+// Note: First argument is a comparison function.
+template <typename Compare, typename... Vectors>
+vector<size_t> multipleSort(Compare comp = Compare(), Vectors&... vectors) {
+    const size_t size = std::get<0>(std::tie(vectors...)).size();
+    ((void)std::initializer_list<int>{(vectors.size() == size ? 0 : 
+        throw std::invalid_argument("Vectors must have the same size"))...});
+
+    std::vector<size_t> indices(size);
+    std::iota(indices.begin(), indices.end(), 0);
+
+    std::sort(indices.begin(), indices.end(), [&](size_t i, size_t j) {
+        return comp(std::tie(vectors[i]...), std::tie(vectors[j]...));
+    });
+
+    auto reorder = [&](auto& vec) {
+        auto temp=vec;
+        for (size_t i = 0; i < size; ++i) {
+            vec[i] = temp[indices[i]];
+        }
+    };
+    (reorder(vectors), ...);
+    return indices;
+}
+
+
+
+// Example usage:
+// aの降順にソートしてbを合わせる
+// vector<pair<int,int>> v{a,b}をsortするのと等価
+// multipleSort(less(),a,b);
+
+// multipleSort(greater(),a,b);
+// vector index=multipleSort(less(),u,v,w);
--- short_templates.cpp
+++ short_templates.cpp
@@ -1,54 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-#if __has_include(<atcoder/all>)
-#include <atcoder/all>
-using namespace atcoder;
-#endif
-// clang-format off
-struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); } }init;
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-typedef vector<vector<int>> Graph;
-
-#define rep(i, x, limit) for (int i = (int)x; i < (int)limit; i++)
-#define REP(i, x, limit) for (int i = (int)x; i <= (int)limit; i++)
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+template<typename T> using minpq=priority_queue<T,vector<T>,greater<T>>;
+#define rep(i, x, limit) for (int i = (x); i < (limit); i++)
+#define REP(i, x, limit) for (int i = (x); i <= (limit); i++)
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define el '\n'
-#define spa " "
-#define YES cout << "Yes" << el
-#define NO cout << "No" << el
-
-const double pi = 3.141592653589793238;
-const int inf = 1073741823;
-const ll infl = 1LL << 60;
-
-//配列の要素を空白区切りで出力
-template<class T> inline void print_vec(const vector<T>& v) {
-    for (int i = 0; i < v.size(); i++) {
-        if(v[i]==inf || v[i]==infl) cout << 'o' << spa;
-        else cout << v[i] << spa;
-    }
-    cout << endl;
-}
-template<class T> inline bool chmax(T& a, T b) {
-    if (a < b) {
-        a = b;
-        return true;
-    }
-    return false;
-}
-template<class T> inline bool chmin(T& a, T b) {
-    if (a > b) {
-        a = b;
-        return true;
-    }
-    return false;
-}
-// clang-format on
+#define spa ' '
+#define Yes cout << "Yes" << el
+#define No cout << "No" << el
 
 ull h,w;
 // 場外判定
