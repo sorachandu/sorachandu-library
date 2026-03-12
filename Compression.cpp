beb84e058ed6a9f7fe462c0db33e6b5a94b0a2b0
sorachandu <sora.chandu28@gmail.com>
2026-03-13 03:41:04 +0900

    Compression.cpp size(),get_from_val()追加、fix

--- Compression.cpp
+++ Compression.cpp
@@ -1,28 +1,31 @@
-/***
- 座標圧縮
- 座標圧縮したいvector<T>を渡す operator[]かgetで圧縮後の値にアクセス
- 構築 O(NlogN), 値取得 O(1)
-***/
+/*//--------------------------------------------------------
+座標圧縮
+座標圧縮したいvector<T>を渡す operator[]かgetで圧縮後の値にアクセス
+構築 O(NlogN), 値取得 O(1)
+*///--------------------------------------------------------
 template<typename T> struct Compression{
     int n;
-    vector<T> val;
-    vector<T> res;
-    vector<int> id;
-    Compression(const vector<T> &a) : n((int)a.size()), val(a), res(a), id(n) {
-        sort(res.begin(),res.end());
-        res.erase(unique(res.begin(),res.end()),res.end());
-        for(int i=0;i<n;i++) id[i]=lower_bound(res.begin(),res.end(),val[i])-res.begin();
+    std::vector<T> cmp;
+    std::vector<int> id;
+    Compression(const std::vector<T> &a) : n(std::ssize(a)), cmp(a), id(n) {
+        std::sort(cmp.begin(),cmp.end());
+        cmp.erase(std::unique(cmp.begin(),cmp.end()),cmp.end());
+        for(int i=0;i<n;i++) id[i]=std::lower_bound(cmp.begin(),cmp.end(),a[i])-cmp.begin();
     }
 
     const int &operator[](int k) const {
         return id[k];
     }
 
-    inline int get(int k) const {
+    int get(int k) const {
         return id[k];
     }
 
-    inline T getRealVal(int k) const {
-        return val[k];
+    int size() const {
+        return ssize(cmp);
+    }
+
+    int get_from_val(T val){
+        return std::lower_bound(cmp.begin(),cmp.end(),val)-cmp.begin();
     }
 };
